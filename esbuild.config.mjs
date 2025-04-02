b21dba456a1acaff3fcf859a273a26bfb6d51d38
szabyy <szabyy72@gmail.com>
2025-04-02 20:37:58 +0200

    init

---
+++ esbuild.config.mjs
@@ -0,0 +1,23 @@
+﻿import { readdirSync, cpSync } from 'fs'
+import { build } from 'esbuild'
+
+cpSync('./build/', './bin/html/', { recursive: true, filter: source => source.endsWith('build') || source.endsWith('.html') });
+
+const files = readdirSync('./build/Scripts/WebSharper/tesztWebSharper3/');
+
+files.forEach(file => {
+  if (file.endsWith('.js')) {
+    var options =
+    {
+      entryPoints: ['./build/Scripts/WebSharper/tesztWebSharper3/' + file],
+      bundle: true,
+      minify: true,
+      format: 'iife',
+      outfile: 'bin/html/Scripts/WebSharper/' + file,
+      globalName: 'wsbundle'
+    };
+
+    console.log("Bundling:", file);
+    build(options);
+  }
+});
